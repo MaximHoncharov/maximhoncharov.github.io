d7a73221ad29796319eff64376e58ff1b6d70452
MaximHoncharov <mgoncharovne@gmail.com>
2021-09-30 19:19:59 +0300

    Add Home Work 12

--- HomeWork_12/script.js
+++ HomeWork_12/script.js
@@ -1,1 +1,22 @@
 "use strict";
+function Cat(name, breed) {
+	this.name = name;
+	this.breed = breed;
+	this.meow = function () {
+		alert("Мяу!");
+	}
+}
+const cat = new Cat("Barsik", "Sphinx");
+cat.meow();
+
+//2
+function User(firstName, lastName, age) {
+	this.firstName = firstName,
+		this.lastName = lastName,
+		this.age = age,
+		this.greet = function () {
+			alert(`Hello, ${this.firstName}`);
+		}
+}
+const user = new User("Vasya", "Petrov", 33);
+user.greet();
