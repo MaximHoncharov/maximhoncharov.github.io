5d0de45ad76958bbf9d1f0fe38ddd4784ed09c7b
MaximHoncharov <mgoncharovne@gmail.com>
2021-10-11 18:40:00 +0300

    Add hw 15

--- HomeWork_15/script.js
+++ HomeWork_15/script.js
@@ -1,3 +1,22 @@
 "use strict";
 
+const arr = [
+	{ name: `Vasya`, surname: `Ivanov` },
+	{ name: `Vanya`, surname: `Ivanov` },
+	{ name: `Albert`, surname: `Vasyliev` },
+]
 
+function keyBy(arr, key) {
+	const newArr = arr.reduce((newArr, user) => {
+		if (!newArr[user[key]]) {
+			newArr[user[key]] = [];
+		}
+		newArr[user[key]].push(user);
+		return newArr;
+	}, {});
+
+	console.log(newArr);
+}
+
+keyBy(arr, `name`);
+keyBy(arr, `surname`);
