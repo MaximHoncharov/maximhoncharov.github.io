5362bfea4cf002fb85f565519c3f0d04d8c67313
MaximHoncharov <mgoncharovne@gmail.com>
2021-10-30 18:13:22 +0300

    Add hw18

--- HomeWork_18/script.js
+++ HomeWork_18/script.js
@@ -1,1 +1,11 @@
 "use strict";
+
+const table = document.querySelector('table');
+const headings = [...table.rows];
+function sortTable(columnIndex) {
+	const values = headings
+		.slice(1)
+		.sort((rowA, rowB) => rowA.cells[columnIndex].innerHTML > rowB.cells[columnIndex].innerHTML ? 1 : -1);
+	table.tBodies[0].append(...values);
+}
+sortTable();
