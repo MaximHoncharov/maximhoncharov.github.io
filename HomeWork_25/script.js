1eeba58387d2bb6d24b7806a65769516abbbe54a
MaximHoncharov <mgoncharovne@gmail.com>
2021-11-25 18:59:45 +0200

    25

--- HomeWork_25/script.js
+++ HomeWork_25/script.js
@@ -6,18 +6,9 @@ String.prototype.reverse = function () {
 }
 
 //2
+Array.prototype.notMutableSorting = Array.prototype.sort;
 Array.prototype.sort = function () {
-	let copyArray = [...this];
-	for (let i = 0; i < copyArray.length; i++) {
-		for (let j = 0; j < copyArray.length - 1 - i; j++) {
-			if (copyArray[j] > copyArray[j + 1]) {
-				const swap = copyArray[j];
-				copyArray[j] = copyArray[j + 1];
-				copyArray[j + 1] = swap;
-			}
-		}
-	}
-	return copyArray;
+	return [...this].notMutableSorting();
 }
 
 //3
