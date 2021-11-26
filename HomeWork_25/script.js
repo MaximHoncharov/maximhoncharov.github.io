91705127f64231f5af4632e019c609ec4b5d0637
MaximHoncharov <mgoncharovne@gmail.com>
2021-11-26 21:06:28 +0200

    25re

--- HomeWork_25/script.js
+++ HomeWork_25/script.js
@@ -7,11 +7,16 @@ String.prototype.reverse = function () {
 
 //2
 Array.prototype.notMutableSorting = Array.prototype.sort;
-Array.prototype.sort = function () {
-	return [...this].notMutableSorting();
+Array.prototype.sort = function (func) {
+	return [...this].notMutableSorting(func);
 }
 
 //3
 NodeList.prototype.map = Array.prototype.map;
 NodeList.prototype.reduce = Array.prototype.reduce;
 NodeList.prototype.find = Array.prototype.find;
+
+//Bonus 
+function ImmutableArray() {
+
+}
