7eb28826d7a7cd14c641579ef59dacc3324c8c4e
MaximHoncharov <mgoncharovne@gmail.com>
2021-11-26 19:56:08 +0200

    26

--- HomeWork_26/script.js
+++ HomeWork_26/script.js
@@ -4,49 +4,70 @@ class Vegetable {
 	constructor(name) {
 		this.name = name;
 	}
-
 }
-
 class Animal {
 	constructor(legs) {
 		this.legs = legs;
 	}
+	stomach = [];
 	eat(food) {
-		this.stomach = [food]
-	}
-	crawl() {
-
-	}
-	walk() {
-
-	}
-	greet() {
-		console.log(`Hi I'm ${firstName + lastName}`)
+		this.stomach.push(food);
+		console.log(`${this.legs} eat food`);
 	}
 }
 
-class Rabbit {
+class Rabbit extends Animal {
 	constructor(breed) {
+		super();
 		this.breed = breed;
 	}
 	jump() {
+		console.log(`${this.breed} jump`);
+	}
+	eat(food) {
+		if (!(food instanceof Vegetable)) {
+			throw new Error("The rabbit only eats vegetables!")
+		}
+		super.eat(food);
 	}
 }
-
-class Snake {
-	constructor(isPoisonous) {
-		this.isPoisonous = isPoisonous
+class Snake extends Animal {
+	constructor(legs, isPoisonous) {
+		super(legs);
+		this.isPoisonous = isPoisonous;
 	}
-
+	crawl() {
+		console.log(`${this.legs} crawling`);
+	}
+	eat(food) {
+		if (!(food instanceof Rabbit)) {
+			throw new Error("The snake only eats rabbits 🐇!")
+		}
+		super.eat(food);
+	}
 }
-
-class Human {
-
+class Human extends Animal {
 	constructor(firstName, lastName) {
-		this.firstName = firstName
-		this.lastName = lastName
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	walk() {
+		console.log(`${this.firstName} walks`);
+	}
+	greet() {
+		console.log(`${this.firstName} ${this.lastName} welcomes you ❤`);
+	}
+	eat(food) {
+		if (food instanceof Human) {
+			throw new Error("We condemn cannibalism!!!");
+		}
+		super.eat(food);
 	}
 }
 
-const oneUser = new Human("Vasya", "Ivanov");
-const twoUser = new Human("Jenya", "Petrov");
+
+
+
+
+
