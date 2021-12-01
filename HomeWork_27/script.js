ad3072b5eb171a8f48e0da185ef7508f2ac31c2d
MaximHoncharov <mgoncharovne@gmail.com>
2021-12-01 23:15:12 +0200

    111

--- HomeWork_27/script.js
+++ HomeWork_27/script.js
@@ -1,6 +1,6 @@
 "use strict";
 //1
-const xhr = new XMLHttpRequest();
+/* const xhr = new XMLHttpRequest();
 xhr.open('GET', '/unstable?maxRandom=20&prob=50');
 xhr.send();
 xhr.addEventListener('load', () => {
@@ -9,7 +9,7 @@ xhr.addEventListener('load', () => {
 	} else {
 		console.log('Hello world '.repeat(xhr.response));
 	}
-})
+}) */
 //2
 function sendRequest(
 	method,
@@ -17,15 +17,17 @@ function sendRequest(
 	{ body, headers = {} } = {},
 	callback
 ) {
-
 	const xhr = new XMLHttpRequest();
 	xhr.open(method, url);
+
 	Object.keys(headers).forEach((key) => {
 		xhr.setRequestHeader(key, headers[key]);
 	});
+
 	xhr.send(body);
+
 	xhr.addEventListener('load', () => {
-		if (xhr.status != 200 && xhr.status != 201 && xhr.status != 204) {
+		if (xhr.status !== 200 && xhr.status !== 201 && xhr.status !== 204) {
 			callback(xhr.response);
 		} else {
 			callback(null, xhr.response);
@@ -34,11 +36,11 @@ function sendRequest(
 }
 
 const user = {
-	firstName: 'Vasya',
-	lastName: 'Ivanov',
-}
+	firstName: 'Rafik',
+	lastName: 'Khachaturyan',
+};
 
-sendRequest('POST', 'https://async-demo.herokuapp.com/objects?prob=20', {
+sendRequest('POST', 'https://async-demo.herokuapp.com/objects?prob=0', {
 	body: JSON.stringify(user),
 	headers: {
 		'Content-type': 'application/json; charset=utf-8',
@@ -48,16 +50,15 @@ sendRequest('POST', 'https://async-demo.herokuapp.com/objects?prob=20', {
 		console.error(err);
 		return;
 	}
-	let received;
 	try {
-		received = JSON.parse(response);
+		response = JSON.parse(response);
 	} catch (err) {
 		console.error(err);
 	}
-	const { id } = received;
+	const { id } = response;
 	const extension = { age: 33 };
 
-	sendRequest('PATCH', `https://async-demo.herokuapp.com/objects/${id}?prob=20`, {
+	sendRequest('PATCH', `https://async-demo.herokuapp.com/objects/${id}?prob=0`, {
 		body: JSON.stringify(extension),
 		headers: {
 			'Content-type': 'application/json; charset=utf-8',
@@ -67,15 +68,13 @@ sendRequest('POST', 'https://async-demo.herokuapp.com/objects?prob=20', {
 			console.error(err);
 			return;
 		}
-
-		sendRequest('DELETE', `https://async-demo.herokuapp.com/objects/${id}?prob=20`, {},
+		sendRequest('DELETE', `https://async-demo.herokuapp.com/objects/${id}?prob=0`, {},
 			(err) => {
 				if (err) {
 					console.error(err);
 				} else {
-					console.log(`Delete user info`);
+					console.log("DELETE!")
 				}
 			});
 	});
 });
-
