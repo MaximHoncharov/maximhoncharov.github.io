7eab40d3c79c1087f01f28277ac5ad08baaacee1
MaximHoncharov <mgoncharovne@gmail.com>
2021-12-01 19:54:45 +0200

    hw27

--- HomeWork_27/script.js
+++ HomeWork_27/script.js
@@ -1,70 +1,56 @@
 "use strict";
-
-/* let inputMinutes = document.querySelector('.input-minutes');
-let inputSeconds = document.querySelector('.input-seconds');
-let button = document.getElementById("btn");
-let timer = document.getElementById("timer");
-let timerSeconds;
-
-button.addEventListener('click', function () {
-	timerSeconds = +(inputSeconds.value) + (+(inputMinutes.value) * 60)
-})
-let startTimer = setInterval(function () {
-
-	let seconds = Math.floor(timerSeconds % 60);
-	let minutes = Math.floor(timerSeconds / 60 % 60);
-	if (timerSeconds === 0) {
-		clearInterval(startTimer);
-		endTime();
+//1
+const xhr = new XMLHttpRequest();
+xhr.open('GET', '/unstable?maxRandom=20&prob=50');
+xhr.send();
+xhr.addEventListener('load', () => {
+	if (xhr.status !== 200) {
+		console.error(xhr.response);
 	} else {
-		let timerValue = `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`
-		timer.innerHTML = timerValue;
+		console.log('Hello world '.repeat(xhr.response));
 	}
-	timerSeconds--;
-}, 1000)
-
-function endTime() {
-	timer.innerHTML = 'TIME OUT';
-	timer.style.color = 'red';
-} */
-
-
-const startTimer = document.querySelector('button');
-startTimer.addEventListener('click', go);
-/* let timeMinutes = +prompt('Minutes', '');
-let timeSeconds = +prompt('Seconds', '');
-alert('Нажми СТАРТ'); */
-let timeMinutes = document.getElementById('input-minutes');
-let timeSeconds = document.getElementById('input-seconds');
-
-function go() {
-	const timer = document.querySelector('h1');
-
-	let timeMinutes = document.getElementById('input-minutes');
-	let timeSeconds = document.getElementById('input-seconds');
-
-	displayTime(timeMinutes, timeSeconds);
-	const countDown = setInterval(() => {
-		timeSeconds--;
-		timer.innerHTML = `${timeMinutes}:${timeSeconds} `;
-		displayTime(timeMinutes, timeSeconds);
-		if (timeSeconds <= 0 && timeMinutes === 0) {
-			endTime();
-			clearInterval(countDown);
-		} else if (timeMinutes !== 0 && timeSeconds === 0) {
-			timeMinutes = (timeMinutes - 1);
-			timeSeconds = 60;
-		}
-	}, 1000);
-
-	function displayTime(timeMinutes, timeSeconds) {
-		let sec = Math.floor(timeSeconds % 60);
-		let min = timeMinutes + Math.floor(timeSeconds / 60);
-		timer.innerHTML = `${min < 10 ? '0' : ''}${min}:${sec < 10 ? '0' : ''}${sec}`;
+})
+//2
+const addUser = new XMLHttpRequest();
+addUser.open("POST", "https://async-demo.herokuapp.com/objects/?prob=20");
+addUser.setRequestHeader("Content-type", "application/json; charset=utf-8");
+const user = JSON.stringify({
+	firstName: "Vasya",
+	lastName: "Ivanov",
+});
+addUser.send(user);
+addUser.addEventListener("load", () => {
+	if (addUser.status !== 201 && addUser.status !== 200 && addUser.status !== 204) {
+		console.error(addUser.response);
+		return;
 	}
-
-	function endTime() {
-		timer.innerHTML = 'TIME OUT';
-		timer.style.color = 'red';
+	try {
+		const userId = JSON.parse(addUser.response);
+		console.log(userId);
+		const addAge = new XMLHttpRequest();
+		addAge.open("PATCH", `https://async-demo.herokuapp.com/objects/${userId.id}/?prob=20`);
+		const age = JSON.stringify({ age: 33 });
+		addAge.setRequestHeader("Content-type", "application/json; charset=utf-8");
+		addAge.send(age);
+		addAge.addEventListener("load", () => {
+			if (addAge.status !== 201 && addAge.status !== 200 && addAge.status !== 204) {
+				console.error(addAge.response);
+				return;
+			}
+			const newUserId = JSON.parse(addAge.response);
+			console.log(newUserId);
+			const deleteUser = new XMLHttpRequest();
+			deleteUser.open("DELETE", `https://async-demo.herokuapp.com/objects/${newUserId.id}/?prob=20`);
+			deleteUser.send();
+			deleteUser.addEventListener("load", () => {
+				if (deleteUser.status !== 201 && deleteUser.status !== 200 && deleteUser.status !== 204) {
+					console.error(deleteUser.response);
+					return;
+				}
+				console.log(`DONE!`);
+			});
+		})
+	} catch (error) {
+		console.error(error);
 	}
-}
+})
